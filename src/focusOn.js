2234a2f0cb14a5a2c4a7159363ad594a166bc18d
Dean Sofer <proloser@hotmail.com>
2015-11-24 04:22:06 -0800

    Added animation to focusOn (and new jQuery dependency)

--- src/focusOn.js
+++ src/focusOn.js
@@ -1,6 +1,8 @@
 /**
  * focusOn - Focuses an input on scope event
  *
+ * @note depends on jQuery
+ *
  * @example
  *   <input focus-on="someEventName">
  *   or
@@ -13,7 +15,7 @@
  *   $scope.$broadcast('anotherEvent');
  *
  */
- angular.module('ts.utils').directive('focusOn', function(){
+ angular.module('ts.utils').directive('focusOn', function($window){
   return {
     link: function($scope, $element, $attrs) {
       var listener = angular.noop;
@@ -21,9 +23,16 @@
         // Stop listening to old event name
         listener();
         // Listen to new event name
-        listener = $scope.$on(newVal, function(){
-          $element[0].scrollIntoView();
-          $element[0].focus();
+        listener = $scope.$on(newVal, function(speed = 1000){
+          // Center element on screen
+          var offset = $element.offset().top - ( $window.innerHeight / 2 ) - ( $element.height() / 2 );
+          $('body').animate({ scrollTop: offset }, {
+            speed: speed,
+            complete: function() {
+              // Focus element (if input)
+              $element[0].focus();
+            }
+          });
         });
       });
     }
