e69f46d3515434e39296a7d1198a14ab91449fcc
Shaun Husain <shaun@intellectual-tech.com>
2016-02-11 15:04:05 -0600

    Modified how the position is calculated to make it more accurate.
    Also pretty big change here is the tooltips are being added to the body
    instead of as a sibling to the element they modify.  When appending
    after the current element there were issues with how the width is
    calculated.  The new method I was able to more precisely line up the
    tooltip with the element and allows the tool tip to resize based on it's
    child contents or have a max width set to allow control of how the text
    wraps.

--- dist/utils.js
+++ dist/utils.js
@@ -72,13 +72,21 @@ angular.module('ts.utils').filter('tsTruncate', function () {
  * @note depends on jQuery
  *
  * @example
- *   <input ts-tooltip="Some message here will be compiled and linked against elements scope" />
+ *
+ *<button
+ *        ts-tooltip-event="click"                              // Options are click or mouseover
+ *        ts-tooltip="Something that shows up in there"         // The text to show in the tooltip
+ *        ts-tooltip-direction="bottom"                         // The direction the tooltip pops up
+ *        ts-tooltip-show="someModel.someBoolean"               // A boolean if set will use this instead of events
+ *        >
+ *   Bottom Click Me
+ * </button>
  *
  */
 
 'use strict';
 
-angular.module('ts.utils').directive('tsTooltip', function ($templateCache, $compile) {
+angular.module('ts.utils').directive('tsTooltip', function ($templateCache, $compile, $window) {
 
   return {
     restrict: 'A',
@@ -88,7 +96,7 @@ angular.module('ts.utils').directive('tsTooltip', function ($templateCache, $com
       tsTooltipEvent: '@',
       tsTooltipShow: '='
     },
-    link: function link($scope, $element, $attrs) {
+    link: function link($scope, $element, $attr) {
       var ARROW_SIZE = 10;
       var template = $templateCache.get('templates/tsTooltip.html');
       var direction = $scope.tsTooltipDirection || 'right';
@@ -101,9 +109,32 @@ angular.module('ts.utils').directive('tsTooltip', function ($templateCache, $com
       var tooltipMain = newTooltip.find("#tooltipMain");
       tooltipMain.addClass(direction);
 
-      console.log(tooltipMain);
+      //$element.after(newTooltip);
+      document.body.insertBefore(newTooltip[0], document.body.childNodes[0]);
 
-      $element.after(newTooltip);
+      function offset(elem) {
+        var docElem, win, rect, doc;
+
+        if (!elem) {
+          return;
+        }
+
+        rect = elem.getBoundingClientRect();
+
+        // Make sure element is not hidden (display: none) or disconnected
+        if (rect.width || rect.height || elem.getClientRects().length) {
+          doc = elem.ownerDocument;
+          win = $window;
+          docElem = doc.documentElement;
+
+          return {
+            top: rect.top + win.pageYOffset - docElem.clientTop,
+            left: rect.left + win.pageXOffset - docElem.clientLeft
+          };
+        }
+      }
+
+      var origOffset = offset(newTooltip[0]);
 
       function makeVisible() {
         if (!isVisible) {
@@ -112,22 +143,33 @@ angular.module('ts.utils').directive('tsTooltip', function ($templateCache, $com
 
           newTooltip[0].style.visibility = 'visible';
 
+          var elementOffset = offset($element[0]);
+
+          //Sets the common top for left and right, or common left for top and bottom
           switch (direction) {
             case 'right':
-              newTooltip[0].style.left = ARROW_SIZE + 'px';
-              newTooltip[0].style.top = -tooltipMain[0].offsetHeight / 2 + 'px';
-              break;
             case 'left':
-              newTooltip[0].style.left = -$element[0].offsetWidth - tooltipMain[0].offsetWidth - ARROW_SIZE + 'px';
-              newTooltip[0].style.top = -tooltipMain[0].offsetHeight / 2 + 'px';
+              newTooltip[0].style.top = elementOffset.top - origOffset.top + $element[0].offsetHeight - tooltipMain[0].offsetHeight / 2 - ARROW_SIZE + 'px';
               break;
             case 'top':
-              newTooltip[0].style.left = -$element[0].offsetWidth / 2 - tooltipMain[0].offsetWidth / 2 + 'px';
-              newTooltip[0].style.top = -$element[0].offsetHeight / 2 - tooltipMain[0].offsetHeight - ARROW_SIZE + 'px';
+            case 'bottom':
+              newTooltip[0].style.left = elementOffset.left - origOffset.left + $element[0].offsetWidth / 2 - tooltipMain[0].offsetWidth / 2 + 'px';
+              break;
+          }
+
+          //Sets the specific left or top values for each direction
+          switch (direction) {
+            case 'right':
+              newTooltip[0].style.left = elementOffset.left - origOffset.left + $element[0].offsetWidth + ARROW_SIZE + 'px';
+              break;
+            case 'left':
+              newTooltip[0].style.left = elementOffset.left - origOffset.left - tooltipMain[0].offsetWidth - ARROW_SIZE + 'px';
+              break;
+            case 'top':
+              newTooltip[0].style.top = elementOffset.top - origOffset.top - tooltipMain[0].offsetHeight - ARROW_SIZE + 'px';
               break;
             case 'bottom':
-              newTooltip[0].style.left = -$element[0].offsetWidth / 2 - tooltipMain[0].offsetWidth / 2 + 'px';
-              newTooltip[0].style.top = $element[0].offsetHeight / 2 + 'px';
+              newTooltip[0].style.top = elementOffset.top - origOffset.top + $element[0].offsetHeight + ARROW_SIZE + 'px';
               break;
           }
         }
@@ -148,7 +190,7 @@ angular.module('ts.utils').directive('tsTooltip', function ($templateCache, $com
         }
       }
 
-      if ($attrs.tsTooltipShow === undefined) {
+      if ($attr.tsTooltipShow === undefined) {
         switch (eventType) {
           case 'mouseenter':
             $element.on('mouseenter', makeVisible);
