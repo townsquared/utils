18dfc1869a680fd35b8bfc6c2eabfff3a0e880a1
Shaun Husain <shaun@intellectual-tech.com>
2016-02-15 03:18:46 -0600

    Updates to get drop downs working in a basic way at least.
    Still needs some finishing up with regard to keyboard event handling and
    customization of list items to allow elements in the list that are not
    selectable and for handling default selection properly.  Does the basics
    of open up allow a user to click an element, has custom templates for
    the list items and a template for the placeholder/chosen value display.
    Intelligently opens up or down depending on the position of the
    element on screen, if more than half way down opens upward and reverses
    the list items, otherwise opens downwards.

--- src/dropDown.js
+++ src/dropDown.js
@@ -16,14 +16,135 @@ angular.module('ts.utils')
     return {
       restrict:'A',
       require: 'ngModel',
-      transclude:true,
+      transclude: {
+        'listItem':'tsListItem',
+        'placeholder':'tsPlaceholder'
+      },
       scope:{
         tsDropDownTemplate:'@',
         tsDropDown: '='
       },
       templateUrl:'templates/tsDropDown.html',
-      link: function($scope, $element, $attr, ngModelCtrl) {
+      link: function($scope, $element, $attr, ngModelCtrl, $transclude) {
+        var selectedItem = 'default selected item';
 
+        var dropDownContainer = angular.element($element.children()[0]);
+        var textDisplayElement = angular.element(dropDownContainer.children()[0]);
+        var dropDownArrow = angular.element(dropDownContainer.children()[1]);
+        var dropDownListContainer = angular.element(dropDownContainer.children()[2]);
+        var dropDownUnorderedList = angular.element($element[0].querySelector('ul'));
+
+        $scope.direction = 'down';
+
+        $scope.dropDownOpen = false;
+
+        $element.on('keydown', function(event){
+          switch(event.keyCode){
+            case 37: //left
+              break;
+            case 38: //up
+              event.preventDefault();
+              break;
+            case 39: //right
+              break;
+            case 40: //down
+              event.preventDefault();
+              break;
+            case 13: //enter
+              toggleDropDown();
+              break;
+          }
+          console.log(event.keyCode)
+        });
+
+        $scope.tsDropDown.forEach(function(dropDownItem){
+          $transclude($scope.$new(), function(clone, scope) {
+            scope.item = dropDownItem;
+
+            var listItem = angular.element(document.createElement('li'));
+            listItem.append(clone[0]);
+
+            listItem.on('click', function(){
+              updateSelected(dropDownItem);
+              $scope.$apply(toggleDropDown);
+            })
+            listItem[0].style.width=(textDisplayElement[0].offsetWidth-12)+'px';
+
+            dropDownUnorderedList.append(listItem);
+          }, null, 'listItem');
+        });
+
+        var placeholderElement, placeholderScope;
+
+        $transclude($scope.$new(), function(clone, scope){
+          placeholderScope = scope;
+          placeholderElement = clone[0];
+
+          textDisplayElement.append(clone[0]);
+        }, null, 'placeholder')
+
+
+        // Take the height of the window divided by 2 to get the middle of the window
+        // if the element's middle is lower than the middle of the window then open upward
+        // otherwise open downward
+        function toggleDropDown(){
+          var rect = $element[0].getBoundingClientRect();
+          var middleOfWindow = window.innerHeight/2;
+          var middleOfElement = rect.top+rect.height/2;
+
+          if(middleOfElement>middleOfWindow){
+            $scope.direction = 'up';
+
+            dropDownListContainer[0].style.bottom = rect.height+'px';
+            dropDownListContainer[0].style.top = 'auto';
+
+            //Flips the items in the list when opening upward
+            for (var i = 0; i < dropDownUnorderedList.children().length; i++) {
+              var childElement = dropDownUnorderedList.children()[i];
+              dropDownUnorderedList.prepend(childElement)
+            }
+          }
+          else{
+            dropDownListContainer[0].style.top = rect.height+'px';
+            dropDownListContainer[0].style.bottom = 'auto';
+            $scope.direction = 'down';
+          }
+
+          $scope.dropDownOpen = !$scope.dropDownOpen;
+        }
+
+        textDisplayElement.on('click', function(){
+          $scope.$apply(toggleDropDown)
+        });
+        dropDownArrow.on('click', function(){
+          $scope.$apply(toggleDropDown)
+        });
+
+        if (!ngModelCtrl) return; // do nothing if no ng-model
+
+        // Specify how UI should be updated
+        ngModelCtrl.$render = function() {
+          //update selected element text
+          updateSelected(ngModelCtrl.$viewValue || '');
+        };
+
+        function updateSelected(selectedValue){
+          placeholderScope.selectedItem = selectedItem = selectedValue;
+          $scope.$evalAsync(read);
+        }
+
+        // Listen for change events to enable binding
+        $element.on('blur keyup', function() {
+
+          $scope.$evalAsync(read);
+          ngModelCtrl.$render();
+        });
+        read(); // initialize
+
+        // Write data to the model
+        function read() {
+          ngModelCtrl.$setViewValue(selectedItem);
+        }
       }
 
     };
